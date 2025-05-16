d53aa310308e4dfbcbf6fb30545875f3cd11bba9
Falguni Patel <33167219+fpcodebase@users.noreply.github.com>
2025-05-16 17:22:06 +0000

    basic class definitions

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -1,17 +1,36 @@
-# Python Object Oriented Programming by Joe Marini course example
 # Basic class definitions
+# Falguni's exercises
 
 
-# TODO: create a basic class
+# basic class
+# This class does nothing
+class example1:
+  pass
+
+class example2:
+  pass
+
+# Class that inherits from other classes
+class example3(example1, example2):
+  pass
+
+# Book class which has all the python class features
 class Book:
+  # Class attributes or variables
+  book_types = ['paper', 'e-book']
+
+  # Class constructor or initializer method
+  # This is an instance method
   def __init__(self, title):
     self.title = title
+    self.type = self.book_types[0]
 
 # TODO: create instances of the class
 book1 = Book("Gora")
 book2 = Book("Science Grade 1 text book")
 
 # TODO: print the class and property
-print(book1)
+print('\n', book1, end='\n\n')
+print("This book is of type: {}".format(book1.type))
 print(book1.title)
 print(book2.title)
